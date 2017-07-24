1bd20563af35b5528b5e81199d7ded6da2d1c686
Grzegorz Dwornicki <gd1100@gmail.com>
2017-07-25 00:49:23 +0200

    fixed entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ do
 done
 ZK_IPs=$(echo $ZK_IPs | sed 's/,,//g')
 
-java -jar ./bin/../target/marathon-assembly-1.4.5.jar --zk zk://${ZK_IPs}/marathon --enable_features $MARATHON_FEATURES --http_port $HTTP_PORT --https_port HTTPS_PORT --master zk://${ZK_IPs}/mesos --hostname $MARATHON_HOSTNAME
+java -jar ./bin/../target/marathon-assembly-1.4.5.jar --zk zk://${ZK_IPs}/marathon --enable_features $MARATHON_FEATURES --http_port $HTTP_PORT --https_port $HTTPS_PORT --master zk://${ZK_IPs}/mesos --hostname $MARATHON_HOSTNAME
