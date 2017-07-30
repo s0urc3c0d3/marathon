8fccebe2d2399e9fd7296730b1aa0b385861495a
Grzegorz Dwornicki <gd1100@gmail.com>
2017-07-30 18:38:39 +0200

    fixed missing awk in get_hosts

--- gen_hosts.sh
+++ gen_hosts.sh
@@ -4,7 +4,7 @@ for stack in $(curl -s rancher-metadata.rancher.internal/2015-12-19/stacks | awk
 do
 	for service in $(curl -s rancher-metadata.rancher.internal/2015-12-19/stacks/${stack}/services | awk -F= '{print $2}')
 	do
-		for container in $(curl -s rancher-metadata.rancher.internal/2015-12-19/stacks/${stack}/services/${service}/containers | -F= '{print $2}')
+		for container in $(curl -s rancher-metadata.rancher.internal/2015-12-19/stacks/${stack}/services/${service}/containers | awk -F= '{print $2}')
 		do
 			echo $(curl -s rancher-metadata.rancher.internal/2015-12-19/stacks/${stack}/services/${service}/containers/${container}/primary_ip) ${stack}-${container} ${container}
 		done
